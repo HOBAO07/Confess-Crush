84da6b1742cc68b174387f12192fd95d0c0b1ff6
HOBAO07 <93861245+HOBAO07@users.noreply.github.com>
2021-11-07 21:50:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'mai mua trà táo qua cho uống nhó bé <3.',
+    messDesc: 'mai mua trà táo qua cho uống nhó bé.',
     btnAccept: 'Okiiiii lun <3',.
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
